830994657b5fd177f6167dc9cdcad069f15bb45b
horix7 <horix77777@gmail.com>
2020-03-05 21:47:18 +0200

    user can edit and Delete a political party feat(edit-party): user can update or delete a party
    >> user admin can delte or update the exosting political party
    
    [Finishes #171568058]
    [Finishes #171568040]

---
+++ UI/js/createParty.js
@@ -0,0 +1,14 @@
+let partyName = document.querySelector('#partyName')
+let partyLeader = document.querySelector('#partyLeader')
+let leaderEmail = document.querySelector('#leaderEmail')
+let officeAddress = document.querySelector('#officeAddress')
+let govId = document.querySelector('#govId')
+let founded = document.querySelector('#founded')
+let mortal = document.querySelector('#mortal')
+let partyId = document.querySelector('#partyId')
+let partyLogo = document.querySelector('#logo')
+let partyDetails = document.querySelector('#details')
+let createBtn = document.querySelector('#createParty')
+
+
+
