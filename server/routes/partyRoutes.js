9e967298e288582b9365742fb2940fd78b8b05ac
horix7 <horix77777@gmail.com>
2020-03-12 09:46:11 +0200

    feat(createParty): user type admin can create a party
    > user admin can create a party with valid required information
    
    [Finishes #171698329]
    [Finishes #171698969]

--- server/routes/partyRoutes.js
+++ server/routes/partyRoutes.js
@@ -3,7 +3,7 @@ import party from '../controllers/partyControlers'
 import express from 'express';
 
 const router = express.Router()
-
+// create party router 
 router.post("/parties",checkToken, party.createParty)
 
 
