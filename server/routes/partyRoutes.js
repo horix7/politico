92f5e5ed0a2faa3d1d6811d82e1a0e2c375cc56a
horix7 <horix77777@gmail.com>
2020-03-12 14:45:36 +0200

    feat(update-delete): admin can update and delete parties and offices
    >user type admin can update office by their id and delete offices by their id as well as deletting and updating parties by thier specific id
    
    [Finishes #171698746]
    [Finishes #171698847]

--- server/routes/partyRoutes.js
+++ server/routes/partyRoutes.js
@@ -8,7 +8,8 @@ const router = express.Router()
 router.post("/parties",checkToken, party.createParty)
 router.get("/parties/:id", checkToken, party.viewParty)
 router.get("/parties", checkToken, party.getAllParties)
-
+router.put("/parties/:id", checkToken, party.editParty)
+router.delete("/parties/:id", checkToken, party.deletePartry)
 
 
 export  default router 
