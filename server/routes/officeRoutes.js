4d70e2854f0063d02f6413bad0bdf8da325385d5
horix7 <horix77777@gmail.com>
2020-03-12 20:09:44 +0200

    feat(deployment): deploying on heroku
    > adding configuration on procfile to deploy on heruko
    
    [Finishes #171774770]

--- server/routes/officeRoutes.js
+++ server/routes/officeRoutes.js
@@ -2,14 +2,15 @@ import checkToken from '../middleware/authentication'
 import office from '../controllers/officeControlers'
 import express from 'express';
 import validation from '../middleware/validation'
+import isAdmin from '../middleware/isAdmin'
 
 const router = express.Router()
 
-router.post("/offices", validation.inputsValids(validation.schemas.OfficeSchema),checkToken, office.createOffice)
+router.post("/offices",checkToken, isAdmin, validation.inputsValids(validation.schemas.OfficeSchema), office.createOffice)
 router.get("/offices/:id", checkToken, office.viewOffice)
 router.get("/offices", checkToken, office.getAllOffices)
-router.put("/offices/:id",validation.inputsValids(validation.schemas.updateOffice),checkToken, office.editOffice)
-router.delete("/offices/:id", checkToken, office.deleteOffice)
+router.put("/offices/:id",checkToken, isAdmin,validation.inputsValids(validation.schemas.updateOffice),office.editOffice)
+router.delete("/offices/:id", checkToken,isAdmin, office.deleteOffice)
 
 export  default router 
 
--- server/routes/userAuthRoutes.js
+++ server/routes/userAuthRoutes.js
@@ -1,12 +1,13 @@
 import user from '../controllers/userCotrnolers'
 import express from 'express';
 import validation from '../middleware/validation'
+import isAdmin from '../middleware/isAdmin'
 
 const router = express.Router()
 
 router.post("/auth/signup",validation.inputsValids(validation.schemas.UserSchema), user.userSignUp)
 router.post("/auth/signin", user.userSignIn)
-router.get("/allusers", user.getAllUsers)
+router.get("/allusers",isAdmin, user.getAllUsers)
 router.get("/useraccount", user.getOneUser)
 
 
